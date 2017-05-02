eea6c899e31193a7c051469d764580749afce958
Jared <jared@bowzed.com>
2017-05-02 15:44:14 +0700

    addDebt

--- contracts/Debt.sol
+++ contracts/Debt.sol
@@ -9,20 +9,19 @@ pragma solidity ^0.4.8;
 
 contract Debt {
   mapping (address => mapping(address => uint)) debts;
+  mapping (address => mapping(address => uint)) pendingDebts;
 
-//  mapping(uint => mapping(uint => uint)) mymap;
-//  mymap[1][2] = 42;
-//  myaddress[friend1address: 5]
+//  debt[debtor][creditor]=55;
+
 function Debt() {
-  debts[0xb5569de6c618b2bab323960d07a096ab191f9640][0xef2c7fd3ce0afd6b57e6966ed3fa4c00fc865062] = 55;
-//  return true;
 }
 
-
-
-  //function addDebt(address user, uint amount) {
-
-  //}
+function addDebt(address debtor, uint amount) returns (uint pendingBalance) {
+  pendingDebts[msg.sender]
+  balanceDebtor = debts[debtor][creditor];
+  balanceCreditor = debts[creditor][debtor];
+  return balance;
+}
 
 
 //  function removeDebt(address user, uint amount)
